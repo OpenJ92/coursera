f1e8381e5bb9784ee6a8524065a291db271d10e8
OpenJ92 <jacob.vartuli.92@gmail.com>
2023-10-27 04:51:22 -0400

    subqueries homework.

---
+++ Database_Engineer/Database_Structure_and_Management/subqueries/run.sql
@@ -0,0 +1,41 @@
+
+source on_enter.sql;
+
+select * 
+from Bookings 
+where BookingSlot > all
+(
+    select BookingSlot 
+    from Bookings 
+    where GuestFirstName = "Vanessa" and GuestLastName = "McCarthy"
+);
+
+select *
+from MenuItems
+where Price > all
+(
+    select Price
+    from MenuItems
+    where Type = "Desserts" and Type = "Starters"
+) and Type <> "Desserts" and Type <> "Starters";
+
+select *
+from MenuItems
+where Price = all
+(
+    select Price
+    from MenuItems
+    inner join Menus on MenuItems.ItemID = Menus.ItemID
+    where Menus.Cuisine = 'Italian' and MenuItems.Type = 'Starters'
+);
+
+select *
+from MenuItems
+where not exists
+(
+    select *
+    from Menus 
+    inner join TableOrders on Menus.MenuID = TableOrders.MenuID and Menus.ItemID = MenuItems.ItemID
+);
+
+source on_exit.sql;
