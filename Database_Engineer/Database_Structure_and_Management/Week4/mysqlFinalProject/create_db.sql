d3296cd40ad7701ec92749b6d51760b02f19eed5
OpenJ92 <jacob.vartuli.92@gmail.com>
2023-11-03 14:17:56 -0400

    Final Project update. Week 1 of Advanced completed

--- Database_Engineer/Database_Structure_and_Management/Week4/mysqlFinalProject/create_db.sql
+++ Database_Engineer/Database_Structure_and_Management/Week4/mysqlFinalProject/create_db.sql
@@ -2,13 +2,13 @@ delimiter //
 create procedure if not exists Little_Lemon.create_db()
 begin
 	create database if not exists Little_Lemon;
-	CREATE TABLE if not exists  Little_Lemon.Customers
+	CREATE TABLE if not exists Little_Lemon.Customers
 	(
 		CustomerID INT NOT NULL PRIMARY KEY,
 	       	FullName VARCHAR(100) NOT NULL,
 	       	PhoneNumber INT NOT NULL UNIQUE
 	);
-	CREATE TABLE if not exists  Little_Lemon.Bookings 
+	CREATE TABLE if not exists Little_Lemon.Bookings 
 	(
 		BookingID INT,
 	       	BookingDate DATE,
@@ -16,12 +16,11 @@ begin
 	       	NumberOfGuests INT,
 		CustomerID INT
 	); 
-	CREATE TABLE  Little_Lemon.Courses 
+	CREATE TABLE if not exists Little_Lemon.Courses 
 	(
 		CourseName VARCHAR(255) PRIMARY KEY,
 	       	Cost Decimal(4,2)
 	);
 end//
 delimiter ;
-
 
