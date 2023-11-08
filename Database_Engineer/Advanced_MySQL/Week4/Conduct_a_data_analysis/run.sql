a1e023a607ff61ca6a23b852a77676d8069245fb
OpenJ92 <jacob.vartuli.92@gmail.com>
2023-11-08 13:24:56 -0500

    Task 3 complete. Trigger with execution

--- Database_Engineer/Advanced_MySQL/Week4/Conduct_a_data_analysis/run.sql
+++ Database_Engineer/Advanced_MySQL/Week4/Conduct_a_data_analysis/run.sql
@@ -13,5 +13,11 @@ set @sold_items_2022 = 0;
 call EvaluateProduct('P1', @sold_items_2020, @sold_items_2021, @sold_items_2022);
 select @sold_items_2020, @sold_items_2021, @sold_items_2022;
 
+-- Task 3
+insert into Orders 
+    (OrderID, ClientID, ProductID, Quantity, Cost, Date)
+values 
+    (100, "CL1", "P1", 10, 100.0, '2020-01-01');
+select * from Audit;
 
-
+drop database Lucky_Shrub;
