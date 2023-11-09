ba6fb11509c19c4f51cc230845cf4b1afb65bcd1
OpenJ92 <jacob.vartuli.92@gmail.com>
2023-11-09 14:41:50 -0500

    task 4 complete. Gather Address data on all persons

--- Database_Engineer/Advanced_MySQL/Week4/Conduct_a_data_analysis/run.sql
+++ Database_Engineer/Advanced_MySQL/Week4/Conduct_a_data_analysis/run.sql
@@ -2,10 +2,10 @@ source create_db.sql;
 source populate_db.sql;
 source analysis.sql;
 
--- Task 1
+-- Task 1 (See: Analysis)
 select FindAverageCost(2022);
 
--- Task 2 (Not outputing correctly. See analysis.sql for more information. FIXED)
+-- Task 2 (See: Analysis)
 set @sold_items_2020 = 0;
 set @sold_items_2021 = 0;
 set @sold_items_2022 = 0;
@@ -13,11 +13,12 @@ set @sold_items_2022 = 0;
 call EvaluateProduct('P1', @sold_items_2020, @sold_items_2021, @sold_items_2022);
 select @sold_items_2020, @sold_items_2021, @sold_items_2022;
 
--- Task 3
+-- Task 3 (See: Analysis)
 insert into Orders 
     (OrderID, ClientID, ProductID, Quantity, Cost, Date)
 values 
     (100, "CL1", "P1", 10, 100.0, '2020-01-01');
 select * from Audit;
 
-drop database Lucky_Shrub;
+-- Task 4 (See: Analysis) make this a procedure?
+call GetPersonsAddresses();
