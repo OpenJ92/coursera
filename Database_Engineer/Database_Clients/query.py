6d78975e9d2c1bae2fdcece2a040d103318654d2
OpenJ92 <jacob.vartuli.92@gmail.com>
2023-11-14 22:02:56 -0500

    Considering general design

--- Database_Engineer/Database_Clients/query.py
+++ Database_Engineer/Database_Clients/query.py
@@ -21,3 +21,29 @@ class QueryFromString(__Query__):
     def query(self):
         return self.string
 
+
+class __CONNECTION__(ABC):
+    @abstractmethod
+    def __enter__(self):
+        pass
+
+    @abstractmethod
+    def __exit__(self):
+        pass
+
+# Next we want to make a class that takes a __Query__ and a 
+# __Connection__ type form and carries out the __Query__ action
+# over the __Connection__. We may want to have secondary set of 
+# query classes which store thier results within class. That is
+# to say that they communicate to the Process Class that they
+# need to have their results stored somehow
+
+# __PROCESS__(__CONNECTION__, [__QUERY__]) 
+# A Process should act out what we've been doing in the lessons
+# so far, making a connection to the db and executing the queries.
+#
+# One might imagine them being written as:
+#
+#   ConstructDatabaseProcess(mysql.connection, construction_queries)
+#   PopulateDatabaseProcess(myql.connection, population_quereis)
+#   AnalyzeDatabase....
