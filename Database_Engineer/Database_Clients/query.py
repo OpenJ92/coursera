b74aa34baa17edaf1c4c74cfdfdaf2a91366c25e
OpenJ92 <jacob.vartuli.92@gmail.com>
2023-11-14 21:39:06 -0500

    some example Query classes with different constructor types

--- Database_Engineer/Database_Clients/query.py
+++ Database_Engineer/Database_Clients/query.py
@@ -1,7 +1,23 @@
 from abc import ABC, property, abstractmethod
 
-class Query(ABC):
-    @property
+class __QUERY__(ABC):
     @abstractmethod
     def query():
         pass
+
+class QueryFromFile(__Query__):
+    def __init__(self, file):
+        self.file = file
+
+    def query(self):
+        with open(self.file, 'r') as q:
+            sql_query = q.read()
+        return sql_query
+
+class QueryFromString(__Query__):
+    def __init__(self, string):
+        self.string
+
+    def query(self):
+        return self.string
+
