b3eb07f26f40a525ceed92c1add65392adf6f69d
Jacob Vartuli-Schonberg <jacob.vartuli.92@gmail.com>
2023-11-16 18:23:25 -0500

    Create employees.sql
    create employees table under create_tables dir. Use in Bundle Query class.

---
+++ Database_Engineer/Database_Clients/query/create_tables/employees.sql
@@ -0,0 +1,9 @@
+CREATE TABLE if not exists Employees (
+  EmployeeID INT AUTO_INCREMENT PRIMARY KEY,
+  Name VARCHAR (255),
+  Role VARCHAR (100),
+  Address VARCHAR (255),
+  Contact_Number INT,
+  Email VARCHAR (255),
+  Annual_Salary VARCHAR (100)
+);
