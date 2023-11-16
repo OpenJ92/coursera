00caa53dd59de2dc97e52c4c829bdae10d2a2658
Jacob Vartuli-Schonberg <jacob.vartuli.92@gmail.com>
2023-11-16 18:33:16 -0500

    Create orders.sql
    populate orders table

---
+++ Database_Engineer/Database_Clients/query/populate_tables/orders.sql
@@ -0,0 +1,7 @@
+INSERT INTO Orders (OrderID, TableNo, MenuID, BookingID, Quantity, BillAmount)
+VALUES
+(1, 12, 1, 1, 2, 86),
+(2, 19, 2, 2, 1, 37),
+(3, 15, 2, 3, 1, 37),
+(4, 5, 3, 4, 1, 40),
+(5, 8, 1, 5, 1, 43);
