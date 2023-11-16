beb618b9fe96a21f73c2bb8e67c718e813458f9f
Jacob Vartuli-Schonberg <jacob.vartuli.92@gmail.com>
2023-11-16 18:29:58 -0500

    POPULATE employee.sql
    populate employees table with given data

---
+++ Database_Engineer/Database_Clients/query/populate_tables/employee.sql
@@ -0,0 +1,7 @@
+REPLACE INTO employees VALUES(EmployeeID, Name, Role, Address, Contact_Number, Email, Annual_Salary)
+(01,'Mario Gollini','Manager','724, Parsley Lane, Old Town, Chicago, IL',351258074,'Mario.g@littlelemon.com','$70,000'),
+(02,'Adrian Gollini','Assistant Manager','334, Dill Square, Lincoln Park, Chicago, IL',351474048,'Adrian.g@littlelemon.com','$65,000'),
+(03,'Giorgos Dioudis','Head Chef','879 Sage Street, West Loop, Chicago, IL',351970582,'Giorgos.d@littlelemon.com','$50,000'),
+(04,'Fatma Kaya','Assistant Chef','132  Bay Lane, Chicago, IL',351963569,'Fatma.k@littlelemon.com','$45,000'),
+(05,'Elena Salvai','Head Waiter','989 Thyme Square, EdgeWater, Chicago, IL',351074198,'Elena.s@littlelemon.com','$40,000'),
+(06,'John Millar','Receptionist','245 Dill Square, Lincoln Park, Chicago, IL',351584508,'John.m@littlelemon.com','$35,000');
