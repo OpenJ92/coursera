095a80f56028fb40c7a67844eaa09a3767a52ae9
Jacob Vartuli-Schonberg <jacob.vartuli.92@gmail.com>
2023-11-16 18:30:46 -0500

    Create bookings.sql
    populate bookings table

---
+++ Database_Engineer/Database_Clients/query/populate_tables/bookings.sql
@@ -0,0 +1,9 @@
+replace INTO Bookings (BookingID, TableNo, GuestFirstName, 
+GuestLastName, BookingSlot, EmployeeID)
+VALUES
+(1, 12, 'Anna','Iversen','19:00:00',1),
+(2, 12, 'Joakim', 'Iversen', '19:00:00', 1),
+(3, 19, 'Vanessa', 'McCarthy', '15:00:00', 3),
+(4, 15, 'Marcos', 'Romero', '17:30:00', 4),
+(5, 5, 'Hiroki', 'Yamane', '18:30:00', 2),
+(6, 8, 'Diana', 'Pinto', '20:00:00', 5);
