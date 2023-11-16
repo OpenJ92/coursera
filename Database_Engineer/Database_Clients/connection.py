3d1006b51d1f0880a8d47a5c273a855db5abc093
Jacob Vartuli-Schonberg <jacob.vartuli.92@gmail.com>
2023-11-16 18:43:53 -0500

    Create connection.py
    see comment on this version of connection.py for more information.

---
+++ Database_Engineer/Database_Clients/connection.py
@@ -0,0 +1,13 @@
+# Here, construct wrappers for db connections. They should implement
+# __enter__ and __exit__ to work with with statements in the python
+# language. These, with __Query__ objects, should compose with 
+# process objects which handle the executeion of the 'raw' sql queries.
+
+class __CONNECTION__(ABC):
+    @abstractmethod
+    def __enter__(self):
+        pass
+
+    @abstractmethod
+    def __exit__(self):
+        pass
